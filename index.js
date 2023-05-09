2aa1fc1d5c64839a93de23425fc89619339b8388
Sayan Biswas <bsayan289@gmail.com>
2023-05-09 21:43:45 +0530

    new mongo store

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
 const passportGoogle = require("./config/passport-google-oauth2-strategy");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 const sassMiddleware = require("node-sass-middleware");
 const flash = require("connect-flash");
 const customMware = require("./config/middleware");
@@ -77,7 +77,7 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 100,
     },
-    store: new MongoStore(
+    store: new MongoStore.create(
       {
         mongooseConnection: db,
         autoRemove: "disabled",
